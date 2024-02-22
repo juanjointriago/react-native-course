b12d05556e09dda06dbb6621a79c875825294037
juanjoitoshiki@gmail.com <juanjoitoshiki@gmail.com>
2024-02-22 17:11:25 -0500

    feat: navigator

--- App.tsx
+++ App.tsx
@@ -1,16 +1,9 @@
 import { View } from 'react-native';
-import { FadeInImage } from './src/components/FadeInImage';
 
 export default function App() {
   return (
+    //navigator
     <View>
-      <FadeInImage uri={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/5.png'}
-        style={{
-          width: 240,
-          height: 300,
-          position: 'absolute',
-          alignSelf: 'center'
-        }} />
     </View>
   );
 }
---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,26 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import { SimplePokemon } from '../interfaces/Pokemon.interface';
+import { HomeScreen } from '../screens/HomeScreen';
+
+
+export type RootStackParams = {
+    HomeScreen: undefined,
+    PokemonScreen: { simplePokemon: SimplePokemon, color: string }
+}
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const Navigator = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+                cardStyle: {
+                    backgroundColor: 'white'
+                }
+            }}>
+            <Stack.Screen name='HomeScreen' component={HomeScreen} />
+            {/* <Stack.Screen name='PokemonScreen' component={PokemonScreen}/> */}
+        </Stack.Navigator>
+    )
+}
