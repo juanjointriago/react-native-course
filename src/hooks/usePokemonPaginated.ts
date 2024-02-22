8b59ae223cfdc2c6819f0a9e064b1733bf88bf64
juanjoitoshiki@gmail.com <juanjoitoshiki@gmail.com>
2024-02-22 16:32:17 -0500

    paginate pokemons

---
+++ src/hooks/usePokemonPaginated.ts
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from "react"
+import { PokemonPaginatedResponse, Result, SimplePokemon } from "../interfaces/Pokemon.interface";
+import { pokemonAPI } from "../api/pokemonAPI";
+
+export const usePokemonPaginated = () => {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
+    const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+    const loadPokemons = async () => {
+        setIsLoading(true);
+        const response = await pokemonAPI.get<PokemonPaginatedResponse>(nextPageUrl.current);
+        nextPageUrl.current = response.data.next;
+        mapPokemonList(response.data.results);
+    }
+
+    const mapPokemonList = (pokemonList: Result[]) => {
+        const newPokemonList: SimplePokemon[] = pokemonList.map(({ name, url }) => {
+            const urlPaths = url.split('/');
+            const id = urlPaths[urlPaths.length - 2];
+            const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+            return { id, picture, name }
+        })
+        setSimplePokemonList([...simplePokemonList, ...newPokemonList]);
+        setIsLoading(false);
+    }
+
+
+    useEffect(() => {
+        loadPokemons();
+    }, [])
+
+
+    return { isLoading, simplePokemonList, loadPokemons }
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,22 @@
+import { FlatList, Image, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 export const HomeScreen = () => {
-
-
-  return (
+    //si se encuantra instalado react native navigation react-native-screens react native safe area context
+    //se puede utilizar el apartado del custom hook useSafeAreaInsets
+    const { top } = useSafeAreaInsets()
+    
+    return (
         <>
+            <Image
+                source={require('../assets/pokebola.png')}
+            />
+            <View
+            style={{alignItems: 'center'}}>
+                {/* <FlatList
+
+                /> */}
+            </View>
         </>
-  )
+    )
 }
