7a44d997f60b9e182a61f23e2e6151b73c101899
juanjoitoshiki@gmail.com <juanjoitoshiki@gmail.com>
2024-02-23 16:31:24 -0500

    feat: changes for paths

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,9 @@
 import { TouchableOpacity, View, Text, Image, StyleSheet, Dimensions } from "react-native";
 import { SimplePokemon } from "../interfaces/Pokemon.interface";
 import { FadeInImage } from "./FadeInImage";
-import ImageColor from 'react-native-image-colors';
 import { useEffect, useRef, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-
-
+import ImageColors from "react-native-image-colors";
 interface Props {
     pokemon: SimplePokemon
 }
@@ -15,27 +13,31 @@ const windowHeigth = Dimensions.get('window').height;
 
 export const PokemonCard = ({ pokemon }: Props) => {
     const [bgColor, setbgColor] = useState('gray');
-    const isMounted =  useRef(true);
+    const isMounted = useRef(true);
     const navigation = useNavigation();
 
-
     useEffect(() => {
-      
-    
-      return () => {
-        
-      }
+        ImageColors.getColors(pokemon.picture, { fallback: 'gray' })
+            .then(colors => {
+                if (!isMounted.current) return;
+                (colors.platform === 'android') ? setbgColor(colors.dominant || 'gray')
+                    : (colors.platform === 'ios') ? setbgColor(colors.background || 'gray')
+                        : setbgColor(colors.vibrant || 'gray')
+            }
+            )
+        return () => {
+            isMounted.current = false;
+        }
     }, [])
-    
 
     return (
         <TouchableOpacity
             activeOpacity={0.9}
             onPress={() => { }} >
             <View
-                style={{...styles.cardContainer, width: windowWidth * 0.4}}>
-                    {/* contenedor del texto */}
-                <View style={{backgroundColor: 'blue'}}>
+                style={{ ...styles.cardContainer, width: windowWidth * 0.4 }}>
+                {/* contenedor del texto */}
+                <View style={{ backgroundColor: 'blue' }}>
                     <Text>
                         {pokemon.name}
                         {'\n#' + pokemon.id}
@@ -53,7 +55,6 @@ export const PokemonCard = ({ pokemon }: Props) => {
     )
 }
 
-
 const styles = StyleSheet.create({
     cardContainer: {
         marginHorizontal: 10,
@@ -62,8 +63,8 @@ const styles = StyleSheet.create({
         marginBottom: 25,
         borderRadius: 10,
         shadowColor: "#000",
-        shadowOffset:{
-            width:0,
+        shadowOffset: {
+            width: 0,
             height: 2,
         },
         shadowOpacity: 0.25,
