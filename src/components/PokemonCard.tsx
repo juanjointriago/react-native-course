63a48272c2975ab5c0dd9cc6159fbdd94dd47e50
juanjoitoshiki@gmail.com <juanjoitoshiki@gmail.com>
2024-02-23 17:56:01 -0500

    feat: pokecard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -34,19 +34,25 @@ export const PokemonCard = ({ pokemon }: Props) => {
             activeOpacity={0.9}
             onPress={() => { }} >
             <View
-                style={{ ...styles.cardContainer, width: windowWidth * 0.4 }}>
+                style={{
+                    ...styles.cardContainer,
+                    width: windowWidth * 0.4,
+                    backgroundColor: bgColor
+                }}>
                 {/* contenedor del texto */}
-                <View style={{ backgroundColor: 'blue' }}>
-                    <Text>
+                <View>
+                    <Text style={styles.name} >
                         {pokemon.name}
                         {'\n#' + pokemon.id}
                     </Text>
                 </View>
-                <View>
+                <View style={styles.pokebolalaContainer}>
                     <Image
-                        source={require('../assets/pokebola-blanca.png')} />
+                        source={require('../assets/pokebola-blanca.png')}
+                        style={styles.pokebola} />
                 </View>
                 <FadeInImage
+                    style={styles.pokemonImage}
                     uri={pokemon.picture}
                 />
             </View>
@@ -69,5 +75,36 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
+    },
+    name: {
+        color: '#ffff',
+        fontSize: 20,
+        fontWeight: 'bold',
+        top: 20,
+        left: 10,
+    },
+    pokebola: {
+        width: 100,
+        height: 100,
+        position: 'absolute',
+        right: -25,
+        bottom: -25
+    },
+    pokebolalaContainer: {
+        width: 100,
+        height: 100,
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        overflow: 'hidden',
+        opacity: 0.5
+    },
+    pokemonImage: {
+        width: 120,
+        height: 120,
+        position: 'absolute',
+        right: -8,
+        bottom: -5
+
     }
 })
