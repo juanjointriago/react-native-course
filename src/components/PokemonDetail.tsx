61b51cf4326e42c835fc8853de489170137745f0
juanjoitoshiki@gmail.com <juanjoitoshiki@gmail.com>
2024-02-26 15:54:31 -0500

    feat: ccss for pokemon screen

--- src/components/PokemonDetail.tsx
+++ src/components/PokemonDetail.tsx
@@ -1,36 +1,35 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { PokemonFull } from '../interfaces/Pokemon.interface';
-interface Props{
+interface Props {
     pokemon: PokemonFull
 }
-export const PokemonDetail = ({pokemon}:Props) => {
+export const PokemonDetail = ({ pokemon }: Props) => {
     return (
         <ScrollView showsVerticalScrollIndicator={false}
             style={{
                 ...StyleSheet.absoluteFillObject,
             }}>
-                <View style={{...styles.container, marginTop: 370}}>
-                    <Text style={styles.title}></Text>
-                    <View style={{flexDirection: 'row'}}>
-                        {
-                            pokemon.types.map(({type})=>(
-                                <Text key={type.name}
-                                 style={{...styles.regularText, marginRight: 10}} >
-                                    {type.name}
-                                </Text>
-                            ))
-                        }
-                    </View>
+            <View style={{ ...styles.container, marginTop: 370 }}>
+                <Text style={styles.title}></Text>
+                <View style={{ flexDirection: 'row' }}>
+                    {
+                        pokemon.types.map(({ type }) => (
+                            <Text key={type.name}
+                                style={{ ...styles.regularText, marginRight: 10 }} >
+                                {type.name}
+                            </Text>
+                        ))
+                    }
                 </View>
-                <View style={{}}>
+            </View>
+            <View style={{}}>
 
-                </View>
+            </View>
         </ScrollView>
     )
 }
-
 const styles = StyleSheet.create({
-    container:{
+    container: {
         marginHorizontal: 20,
     },
     title: {
--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,16 +1,50 @@
 import { StackScreenProps } from "@react-navigation/stack"
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 import { RootStackParams } from "../navigator/Navigator"
 
 interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'>{};
-
 export const PokemonScreen = ({navigation, route}:Props) => {
-
   const {simplePokemon, color} = route.params;
 
   return (
-    <View>
-        <Text>{simplePokemon.name}</Text>
+    <View style={{flex:1}}>
+        <Text >{simplePokemon.name}</Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  headerContainer:{
+    height: 370,
+    zIndex: 999,
+    alignItems: 'center',
+    borderBottomRightRadius: 1000,
+    borderBottomLeftRadius: 1000,
+  },
+  backButton:{
+    position: 'absolute',
+    left:20,
+  },
+  pokemonName:{
+    color: '#000000',
+    fontSize: 40,
+    alignSelf: 'flex-start',
+    left: 20
+  },
+  pokeball:{
+    width: 250,
+    height: 250,
+    bottom: -20,
+    opacity: 0.7,
+  },
+  pokemonImage: {
+    width:250,
+    height: 250,
+    position: 'absolute',
+    bottom: -15,
+  },
+  loadingIndicator:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
